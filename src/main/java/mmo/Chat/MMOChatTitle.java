b399cc9b9d9e7ab88efd822182c0c6a8a591bd16
Robin Cloutman <rycochet@rycochet.com>
2012-03-07 03:21:28 +0000

    Update readme, pom, and move to new event system.

--- src/main/java/mmo/Chat/MMOChatTitle.java
+++ src/main/java/mmo/Chat/MMOChatTitle.java
@@ -16,34 +16,32 @@
  */
 package mmo.Chat;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import mmo.Core.ChatAPI.MMOChatEvent;
 import mmo.Core.MMO;
-import mmo.Core.MMOListener;
 import mmo.Core.MMOPlugin;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.event.Event.Priority;
-import org.bukkit.event.Event.Type;
-import org.bukkit.util.config.Configuration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 
-public class MMOChatTitle extends MMOPlugin {
+public class MMOChatTitle extends MMOPlugin implements Listener {
 
-	static public boolean config_always_show = false;
-	static public int config_max_titles = 1;
-	static public String config_stop_prefix = "!!!";
-	static public List<String> config_default = new ArrayList<String>();
-	static public Map<String, String> default_perms = new LinkedHashMap<String, String>();
+	public static boolean config_always_show = false;
+	public static int config_max_titles = 1;
+	public static String config_stop_prefix = "!!!";
+	public static List<String> config_default = new ArrayList<String>();
+	public static Map<String, String> default_perms = new LinkedHashMap<String, String>();
 
 	@Override
-	public void loadConfiguration(Configuration cfg) {
+	public void loadConfiguration(final FileConfiguration cfg) {
 		config_always_show = cfg.getBoolean("always_show", config_always_show);
 		config_max_titles = cfg.getInt("max_titles", config_max_titles);
 		config_stop_prefix = cfg.getString("stop_prefix", config_stop_prefix);
-		config_default = cfg.getStringList("default", config_default);
+		if (cfg.isList("default")) {
+			config_default = cfg.getStringList("default");
+		}
 		default_perms.clear();
 		for (String arg : config_default) {
 			String[] perm = arg.split("=");
@@ -56,47 +54,45 @@ public class MMOChatTitle extends MMOPlugin {
 	@Override
 	public void onEnable() {
 		super.onEnable();
-		pm.registerEvent(Type.CUSTOM_EVENT,
-				new MMOListener() {
+		pm.registerEvents(this, this);
+	}
 
-					@Override
-					public void onMMOChat(MMOChatEvent event) {
-						if (config_always_show || event.hasFilter("Title")) {
-							LinkedList<String> titles = new LinkedList<String>();
-							Player from = event.getPlayer();
-							Map<String, String> perms = new LinkedHashMap<String, String>(default_perms);
-							for (String arg : event.getArgs("Title")) {
-								String[] perm = arg.split("=");
-								if (perm.length == 2) {
-									perms.put(perm[0], perm[1]);
-								}
-							}
-							for (String arg : perms.keySet()) {
-								boolean end = false;
-								if (from.hasPermission(arg)) {
-									String title = perms.get(arg);
-									if (!config_stop_prefix.isEmpty() && title.startsWith(config_stop_prefix)) {
-										end = true;
-										title = title.substring(config_stop_prefix.length()).trim();
-									}
-									if (!title.isEmpty()) {
-										titles.add(title);
-									}
-									if (config_max_titles > 0 && titles.size() >= config_max_titles) {
-										end = true;
-									}
-								}
-								if (end) {
-									break;
-								}
-							}
-							if (!titles.isEmpty()) {
-								for (Player to : event.getRecipients()) {
-									event.setFormat(to, event.getFormat(to).replaceAll("%2\\$s", MMO.join(titles, " ") + " %2\\$s"));
-								}
-							}
-						}
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onMMOChat(final MMOChatEvent event) {
+		if (config_always_show || event.hasFilter("Title")) {
+			final List<String> titles = new LinkedList<String>();
+			final Player from = event.getPlayer();
+			final Map<String, String> perms = new LinkedHashMap<String, String>(default_perms);
+			for (String arg : event.getArgs("Title")) {
+				final String[] perm = arg.split("=");
+				if (perm.length == 2) {
+					perms.put(perm[0], perm[1]);
+				}
+			}
+			for (String arg : perms.keySet()) {
+				boolean end = false;
+				if (from.hasPermission(arg)) {
+					String title = perms.get(arg);
+					if (!config_stop_prefix.isEmpty() && title.startsWith(config_stop_prefix)) {
+						end = true;
+						title = title.substring(config_stop_prefix.length()).trim();
 					}
-				}, Priority.High, this);
+					if (!title.isEmpty()) {
+						titles.add(title);
+					}
+					if (config_max_titles > 0 && titles.size() >= config_max_titles) {
+						end = true;
+					}
+				}
+				if (end) {
+					break;
+				}
+			}
+			if (!titles.isEmpty()) {
+				for (Player to : event.getRecipients()) {
+					event.setFormat(to, event.getFormat(to).replaceAll("%2\\$s", MMO.join(titles, " ") + " %2\\$s"));
+				}
+			}
+		}
 	}
 }
