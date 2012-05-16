d82730b23fdea8a1268073c3da5e3baa32b821e1
Wulfspider <hello@wulfspider.com>
2012-05-16 11:22:18 -0400

    Updated pom.xml, readme, plugin.yml, and added header.txt. Added automatic license header checking and formatting. Formatted headers to be project specific. A little bit more formatting on misc files.

--- src/main/java/mmo/Chat/MMOChatTitle.java
+++ src/main/java/mmo/Chat/MMOChatTitle.java
@@ -1,7 +1,7 @@
 /*
- * This file is part of mmoMinecraft (https://github.com/mmoMinecraftDev).
+ * This file is part of mmoChatTitle <http://github.com/mmoMinecraftDev/mmoChatTitle>.
  *
- * mmoMinecraft is free software: you can redistribute it and/or modify
+ * mmoChatTitle is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
